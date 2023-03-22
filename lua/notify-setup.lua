f39f0610df6bdb54fd27f082e0a9271e2bfbf7c6
Breno Prata <brenoprata@hotmail.com>
2023-03-22 09:36:36 +0100

    Remove LSP message

--- lua/notify-setup.lua
+++ lua/notify-setup.lua
@@ -4,7 +4,16 @@ notify.setup {
 	background_colour = '#161421'
 }
 
-vim.notify = notify
+local banned_messages = { "No information available" }
+vim.notify = function(msg, ...)
+  for _, banned in ipairs(banned_messages) do
+    if msg == banned then
+      return
+    end
+  end
+  require("notify")(msg, ...)
+end
+
 vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
 	local client = vim.lsp.get_client_by_id(ctx.client_id)
   	local lvl = ({
@@ -13,6 +22,7 @@ vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
    		'INFO',
     		'DEBUG',
   	})[result.type]
+
  	notify({ result.message }, lvl, {
     		title = 'LSP | ' .. client.name,
     		timeout = 10000,
