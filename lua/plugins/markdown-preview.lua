5a0ddf99da4f868f3d820a7cd4219c16dfcbb398
Breno Prata <brenoprata@hotmail.com>
2025-03-13 13:01:44 +0100

    Migrate AI plugins to lazy

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,6 +1,21 @@
 return {
 	{
 		"OXY2DEV/markview.nvim",
-		lazy = false
+		lazy = false,
+		dependencies = {"echasnovski/mini.nvim"},
+		opts = {
+			preview = {
+				filetypes = { "markdown", "codecompanion", "Avante" },
+				ignore_buftypes = {},
+			},
+			display = {
+				diff = {
+					enabled = true,
+					layout = "vertical", -- vertical|horizontal split for default provider
+					opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+					provider = "mini_diff", -- default|mini_diff
+				},
+			}
+		}
 	}
 }
