3508eca159493bee3d54552662677bed033f1aa5
Breno Prata <brenoprata@hotmail.com>
2025-03-13 18:09:29 +0100

    Initialize highlight colors

--- lua/plugins/highlight-colors.lua
+++ lua/plugins/highlight-colors.lua
@@ -1,15 +1,16 @@
 return {
 	{
 		"brenoprata10/nvim-highlight-colors",
+		lazy = false,
 		opts = {
 			enable_tailwind = true,
 			render = 'virtual',
 			exclude_buffer = function(bufnr)
-				return vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr)) > 1000000 
+				return vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr)) > 1000000
 			end,
 			custom_colors = {
 				{label = '%-%-theme%-background%-color', color = '#23222f'},
 			}
-		}
+		},
 	}
 }
