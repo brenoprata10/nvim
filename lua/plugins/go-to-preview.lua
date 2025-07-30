ec2d6a2e62598f28943b9d19fad12084cb0f0975
Breno Prata <brenoprata@hotmail.com>
2025-07-30 21:53:28 +0200

    fix go to preview

--- lua/plugins/go-to-preview.lua
+++ lua/plugins/go-to-preview.lua
@@ -1,6 +1,11 @@
 return {
 	{
 		"rmagatti/goto-preview",
-		opts = {}
+		opts = {},
+		keys = {
+			{ "gpd", "<cmd>lua require('goto-preview').goto_preview_definition()<cr>", nowait = true, desc = "Go to Preview" },
+			{ "gpi","<cmd>lua require('goto-preview').goto_preview_implementation()<cr>", desc = "Goto Preview Definition" },
+			{ "gpc","<cmd>lua require('goto-preview').close_all_win()<cr>", desc = "Close Goto Preview" },
+		}
 	}
 }
