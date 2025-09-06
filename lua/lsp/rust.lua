543c0b2a3781dd28fb7092e85414d8d534a9c000
Breno Prata <brenoprata@hotmail.com>
2025-09-06 21:07:13 +0200

    use rust fmt with each save

--- lua/lsp/rust.lua
+++ lua/lsp/rust.lua
@@ -4,7 +4,21 @@ local attach_common = require("attach-common")
 
 lsp.rust_analyzer.setup({
 	capabilities = capabilities,
-    	on_attach = function(client, bufnr)
+	settings = {
+		['rust-analyzer'] = {
+			checkOnSave = {
+				command = 'clippy', -- Optional: run clippy on save
+			},
+		},
+	},
+	on_attach = function(client, bufnr)
+		-- Format on save using LSP
+		vim.api.nvim_create_autocmd('BufWritePre', {
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
 		attach_common.setup(client, bufnr)
-    	end
+	end
 })
